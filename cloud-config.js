c26ab531a11b201e13b21661943d5d60d190aba3
jaepil0514 <Jaepil89@gmail.com>
2026-06-12 13:13:04 +1000

    Create cloud-config.js

---
+++ cloud-config.js
@@ -0,0 +1,6 @@
+window.BATCH_BREW_CLOUD = {
+  mode: "supabase",
+  supabaseUrl: "https://hvkrksymwzqhihxbmqnl.supabase.co",
+  supabaseAnonKey: "sb_publishable_ztQAOXPmN60DHpIxLEVU2Q_4SizSzjS",
+  table: "batch_brew_records"
+};
